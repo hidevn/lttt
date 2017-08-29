09efca5edae45ca878cf971ccb18d2e0fffff577
DESKTOP-JCHPSP3\ASUS <hidevn@yahoo.com>
2017-08-29 20:58:24 +0700

    lag

--- bai12.m
+++
@@ -1,6 +0,0 @@
-a = 36;
-b = 15;
-disp(mod(a,b));
-disp(rem(a,b));
-disp(gcd(a,b));
-disp(lcm(a,b));
--- bai16.m
+++
@@ -1,9 +0,0 @@
-a = [1 0 2];
-b = [0 2 2];
-disp(a=b);
-disp(a<b);
-disp(a<b<a);
-disp(a<b<b);
-disp(a|(a));
-disp(b&(b));
-disp(a=b==a);
