8332f212a910faf56338b184d58cae7c11f5d384
DESKTOP-JCHPSP3\ASUS <hidevn@yahoo.com>
2017-08-29 20:53:06 +0700

    xóa bớt mấy cái không cần thiết

--- Bai10.m
+++
@@ -1,9 +0,0 @@
-x=randperm(35);
-fprintf('x = \n');
-disp(x);
-fprintf('y(x) = \n');
-y = x;
-y(y<6) = 2;
-y((y >= 6) & (y <=20)) = y((y >= 6) & (y <=20)) - 4;
-y(y >= 20 & y <=25) = 36 - y(y >= 20 & y <=25);
-disp(y);
