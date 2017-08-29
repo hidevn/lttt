c1fc74e6b9e74f8cb54dff50808f6521d40a70bb
Chien.nd <zxzmrchien@gmail.com>
2017-08-29 22:02:17 +0700

    file loz

---
+++ Bai18.m
@@ -0,0 +1,17 @@
+x = [1 4 8];
+y = [2 1 5];
+A = [2 7 9 7 ; 3 1 5 6 ; 8 1 2 5];
+fprintf('1.8.2) \n');
+disp( [x;y]);
+fprintf('1.8.3) \n');
+disp( A(:,[1 4]));
+fprintf('1.8.4) \n');
+disp( A([2 3], [3 1]));
+fprintf('1.8.5) \n');
+disp( A(:));
+fprintf('1.8.6) \n');
+disp( [A;A(end,:)]);
+fprintf('1.8.7) \n');
+disp( A(1:3,:));
+fprintf('1.8.8) \n');
+disp( [A;A(1:2,:)]);
