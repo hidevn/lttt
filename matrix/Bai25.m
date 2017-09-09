7abed0441fdd0e7f0ab546ece82d78ee4f2ed271
Chien.nd <zxzmrchien@gmail.com>
2017-09-09 20:11:47 +0700

    abcdef

--- matrix/Bai25.m
+++ matrix/Bai25.m
@@ -20,3 +20,18 @@ fprintf('A*B = \n');
 disp(A*B);
 fprintf('[A11*B11+A12*B21 A11*B12+A12*B22;\n A21*B11+A22*B21 A21*B12+A22*B22] = \n');
 disp([A11*B11+A12*B21 A11*B12+A12*B22; A21*B11+A22*B21 A21*B12+A22*B22])
+%2.5.4;
+% De thay dieu phai chung minh
+%2.5.5;
+% Dieu kien de co [A11 A12; A21 A22]* B[11 B12; B21 B22] 
+%  = [A11B11 + A12B21 A11B12 + A12B22; A21B11 + A22B21 A21B12 + A22B22]
+% la A11 la ma tran co a*m
+% A12 la ma tran co a*n
+% A21 la ma tran co b*m
+% A22 la ma tran co b*n
+% B11 la ma tran co m*p
+% B12 la ma tran co m*q
+% B21 la ma tran co n*p
+% B22 la ma tran co n*q
+% 2.5.6;
+% Su dung nhan khoi ma tran de dang thu duoc ket qua can chung minh;
---
+++ matrix/Bai27.m
@@ -0,0 +1,12 @@
+% Ta chung minh bang quy nap
+% Voi A la ma tran tam giac tren va khong suy bien, de thay 
+% A^(-1) cung la mot ma tran tam giac tren
+% gia su dung voi n = k, ta can chung minh no cung dung voi n =k +1
+% voi n=k+1, ta co A co dang [A11 A12; 0 a]
+% trong do A11 la ma tran tam giac tren kich co k*k, A12 la ma tran kich co
+% k*1, a la ma tran kich co 1*1
+% tu ket qua cua bai 2.6, ta co
+% A^(-1) = [ A11^(-1) -A11^(-1)*A12*1/a; 0 1/a]
+% theo gia thiet quy nap ta co A11^(-1) la ma tran tam giac tren 
+% => A^(-1) cung la ma tran tam giac tren
+% => dieu phai chung minh
